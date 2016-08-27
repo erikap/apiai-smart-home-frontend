0a02f50c086ce296a5891479645e8cf2302f81c0
Erika Pauwels <erika.pauwels@gmail.com>
2016-08-27 22:17:10 +0200

    Lists thermostats with an editable temperature
    Thermostats are retrieved from a static configured list. They should be
    fetched from a service in the future.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Thermostat } from './thermostat';
 
+const THERMOSTATS: Thermostat[] = [
+  { id: 11, temperature: 20, location: 'Living room' },
+  { id: 12, temperature: 21, location: 'Kitchen' },
+  { id: 13, temperature: 17, location: 'Bedroom' }  
+];
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.sass']
 })
 export class AppComponent {
-  title = 'app works!';
+  thermostats = THERMOSTATS;
 }
