4a20dd1d3ba1425a90a55144a484846577f68960
Erika Pauwels <erika.pauwels@gmail.com>
2016-08-29 22:48:38 +0100

    Add voice recognition to chat input field

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { Message } from '../message';
 import { SmartChatService } from '../smart-chat.service';
+
+export interface IWindow extends Window {
+  webkitSpeechRecognition: any;
+}
 
 @Component({
   selector: 'app-home',
@@ -9,9 +13,16 @@ import { SmartChatService } from '../smart-chat.service';
 })
 export class HomeComponent implements OnInit {
 
-  messages: Message[];
+  zone: NgZone;
   
-  constructor(private smartChatService : SmartChatService) { }
+  messages: Message[];
+
+  chat: String = '';
+  recordingIcon: String = 'mic_none';
+  
+  constructor(private smartChatService : SmartChatService, zone : NgZone) {
+    this.zone = zone;
+  }
 
   ngOnInit() {
     this.messages = this.smartChatService.getMessages();
@@ -19,6 +30,35 @@ export class HomeComponent implements OnInit {
 
   submitMessage(value: string): void {
     this.smartChatService.submitMessage({text: value, sender: 'You'});
+    this.chat = '';
   }
 
+  startVoiceRecording(): void {
+    if ('webkitSpeechRecognition' in window) {
+      const { webkitSpeechRecognition } : IWindow = <IWindow>window;
+      const recognition = new webkitSpeechRecognition();
+
+      recognition.continuous = false;
+      recognition.interimResults = false;
+      recognition.lang = "en-US";
+      
+      recognition.start();
+      this.recordingIcon = 'mic';
+ 
+      recognition.onresult = (e) => {
+        console.log('Transcript is: ' + e.results[0][0].transcript);
+	this.zone.run( () => {
+          this.recordingIcon = 'mic_none';
+          this.smartChatService.submitMessage({text: e.results[0][0].transcript, sender: 'You'});
+	});
+        recognition.stop();
+      };
+ 
+      recognition.onerror = (e) => {
+        console.log('An error occured: [' + e.error + '] ' + e.message);
+        recognition.stop();
+        this.zone.run( () => { this.recordingIcon = 'mic_none'; });
+      }
+    }
+  }
 }
